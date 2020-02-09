8813ba0a1ce89c1e87d3f9c161a55da807ba5071
Alex Thewsey <athewsey@gmail.com>
2020-02-09 11:16:02 +0000

    feat(smproc): parallel processing

--- sm_processing/getframes.py
+++ sm_processing/getframes.py
@@ -2,6 +2,7 @@
 
 # Built-Ins:
 import argparse
+import json
 import os
 import shutil
 import subprocess
@@ -30,6 +31,10 @@ def existing_folder_arg(raw_value):
         raise argparse.ArgumentTypeError("%s is not a directory" % value)
     return value
 
+def list_arg(raw_value):
+    """argparse type for a list of strings"""
+    return str(raw_value).split(",")
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Extract frame images from video files")
     parser.add_argument("--input", type=existing_folder_arg,
@@ -43,16 +48,43 @@ def parse_args():
         help="(Approximate) number of frames per second to save, or save every frame if 0."
     )
     
+    # Unlike in training jobs (which have `SM_HOSTS` and `SM_CURRENT_HOST` env vars), processing
+    # jobs need to refer to the resource config file to determine how many instances are running
+    # and which index we are:
+    resconfig = {}
+    try:
+        with open("/opt/ml/config/resourceconfig.json", "r") as cfgfile:
+            resconfig = json.load(cfgfile)
+    except FileNotFoundError:
+        print("/opt/ml/config/resourceconfig.json not found: default to one instance")
+        pass # Ignore
+
+    # In case the config file is not found (e.g. for local running), the parallelization config can
+    # be provided through CLI for convenience:
+    parser.add_argument("--hosts", type=list_arg,
+        default=resconfig.get("hosts", ["unknown"]),
+        help="Comma-separated list of host names running the job"
+    )
+    parser.add_argument("--current-host", type=str,
+        default=resconfig.get("current_host", "unknown"),
+        help="Name of this host running the job"
+    )
+
     return parser.parse_args()
 
-def extract_frames(src, dest, fps=0):
+def extract_frames(src, dest, fps=0, shard_ix=0, shard_count=1):
     # Getting the FPS of a video is major_ver dependent in OpenCV:
     (cv_major_ver, _, _) = (cv2.__version__).split('.')
     
     if fps != 0:
         raise NotImplementedError("FPS is a parameter, but not yet implemented!")
     
-    for filename in os.listdir(src):
+    for ix, filename in enumerate(sorted(os.listdir(src))):
+        # Simple/naive parallelization:
+        # (Note 0 % anything = 0, so need to offset to 1-based ix)
+        if (ix + 1) % shard_count != shard_ix:
+            continue
+
         vidid, _, extension = filename.rpartition(".")
         if (extension not in SUPPORTED_EXTENSIONS):
             print(f"Skipping non-video file {filename}")
@@ -85,7 +117,16 @@ def extract_frames(src, dest, fps=0):
 
 if __name__ == "__main__":
     args = parse_args()
+    print("Loaded arguments:")
     print(args)
+    print("Environment variables:")
     print(os.environ)
+
     os.makedirs(args.output, exist_ok=True)
-    extract_frames(args.input, args.output, fps=args.frames_per_second)
+    extract_frames(
+        args.input,
+        args.output,
+        fps=args.frames_per_second,
+        shard_ix=args.hosts.index(args.current_host),
+        shard_count=len(args.hosts)
+    )
